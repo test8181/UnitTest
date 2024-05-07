089eb562e49a5f604fb8713651157f0e2ba7e463
DTMLLUAdminUser <srbdtest81@gmail.com>
2024-05-07 18:31:18 +0600

    Test Project added

---
+++ SampleCalcTest/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleCalc;
+
+namespace SampleCalcTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculator _calculator = new Calculator();
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(_calculator.Add(5, 2), 5 + 2);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Assert.AreEqual(_calculator.Sub(5, 2), 5 - 2);
+        }
+    }
+}
